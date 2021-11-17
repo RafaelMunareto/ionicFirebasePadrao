8bcc512de1ef2838364ce81b208cdf877527b08e
Rafael Munareto <rafael.munareto@caixa.gov.br>
2021-11-17 17:49:23 -0300

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase:  {
-    apiKey: "AIzaSyBwwlsvR3tx246gX6WZ1LKcA4ljuftiCxA",
-    authDomain: "quizvarejo.firebaseapp.com",
-    projectId: "quizvarejo",
-    storageBucket: "quizvarejo.appspot.com",
-    messagingSenderId: "1038112587009",
-    appId: "1:1038112587009:web:00aea9c37c6df00501af00",
-    measurementId: "G-XTZ7TW4LLQ"
+    // COLOQUE A CHAVE DO FIREBASE AQUI
   }
 };
